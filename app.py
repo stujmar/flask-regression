097d3381db08d029d1a8bd95ab61a559fc582ba0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-11 21:25:58 -0500

    [sm] got a get home route working and returning a little html

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import flask
 
-print('hello from a flask app', flask.__version__)
+print('... loading flask app ...', flask.__version__)
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+app.run()
