5ca0d490c9e6f16e25a3eec3b465b57119f04e27
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-12 20:28:04 -0500

    [sm] imported render templates and made a route that returns html based on a template

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import requests 
 
 print('... loading flask app ...', flask.__version__)
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/hello')
+def hello():
+    # Flask knows to look in templates folder.
+    return render_template('index.html')
 
 # @app.route('/', methods=['GET'])
 # def home():
