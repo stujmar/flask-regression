43d9a683ba8fe9b464f9227a065ecdcf5c010604
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-12 19:35:49 -0500

    [sm] brought in some reference post endpoints

--- app.py
+++ app.py
@@ -10,4 +10,14 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+@app.route('/api/post/<uuid>', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.json
+    print(content)
+    return uuid
+
+@app.route('api/tree-data', methods=['POST'])
+def tree_data():
+    return flask.jsonify({"data": "test"})
+
 app.run()
