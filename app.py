b3d350f4d4351c0851d404816944ca39fb177daa
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-12 20:11:09 -0500

    [sm] rewrote the index route and was able to return a hello world

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ print('... loading flask app ...', flask.__version__)
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('/api/post/<uuid>', methods=['GET', 'POST'])
-def add_message(uuid):
-    content = request.json
-    print(content)
-    return uuid
+# @app.route('/', methods=['GET'])
+# def home():
+#     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('api/tree-data', methods=['POST'])
-def tree_data():
-    return flask.jsonify({"data": "test"})
+# @app.route('/api/post/<uuid>', methods=['GET', 'POST'])
+# def add_message(uuid):
+#     content = request.json
+#     print(content)
+#     return uuid
+
+# @app.route('api/tree-data', methods=['POST'])
+# def tree_data():
+#     return flask.jsonify({"data": "test"})
 
 app.run()
