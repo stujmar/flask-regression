81aeed43942c8b2fec80b79327aeda5f53829484
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-19 20:09:10 -0500

    [sm] got updating tasks to work

--- app.py
+++ app.py
@@ -59,7 +59,12 @@ def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-       pass
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
     else: 
         return render_template('update.html', task=task)
 
